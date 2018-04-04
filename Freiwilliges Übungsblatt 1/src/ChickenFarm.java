f2fc9ffddcae826c6fc739295871b453e7d6e5dd
student <student@192.168.101.20>
2018-04-04 10:23:14 +0200

    up to date

--- Freiwilliges Übungsblatt 1/src/ChickenFarm.java
+++ Freiwilliges Übungsblatt 1/src/ChickenFarm.java
@@ -4,6 +4,7 @@ public class ChickenFarm {
 	private int [][] eggBoxStack;
 	private int minimumOrderSize;
 	private int maximumOrderSize;
+	private int exception=5;
 	
 	public ChickenFarm() {
 		minimumOrderSize=25;
@@ -15,14 +16,43 @@ public class ChickenFarm {
 		this.maximumOrderSize=maximumOrderSize;
 	}
 	
-	public int[][] fillEggBoxStack(int quanOfEgg) {
-		int quanFullBox=quanOfEgg/25;
-		int quanLastBox=quanOfEgg%25;
-		int planEggBoxStack[][]=new int [quanFullBox][quanLastBox];
-		return planEggBoxStack[][];
+	private Egg[][] createEggBoxStack(int quanOfEgg) {
+	int quanFullBox=quanOfEgg/25;
+	int quanLastBox=quanOfEgg%25;
+	if (quanLastBox==0) {
+		Egg orgEggBoxStack[][]=new Egg [quanFullBox][];
+		for (int i = 0; i<quanFullBox;i++) {
+			orgEggBoxStack[i]=new Egg [25];
+		}return orgEggBoxStack;
+		
+	}else {
+		Egg orgEggBoxStack[][]=new Egg [quanFullBox+1][];
+		for (int i = 0; i<quanFullBox;i++) {
+			orgEggBoxStack[i]=new Egg[25];
+		}
+		orgEggBoxStack[quanFullBox]=new Egg[quanLastBox];
+		return orgEggBoxStack;
+		
 	}
 	
-	public Egg[][] deliverEgg(quantity) {
-		if()
+}
+	
+	
+
+	public Egg[][] deliverEgg(int quanOfEgg) {
+		Egg[][] eggBoxStack=createEggBoxStack(quanOfEgg);
+		for(int i=0;i<eggBoxStack.length;i++) {
+			for(int j=0; j<(eggBoxStack[i].length);j++) {
+				eggBoxStack[i][j]=new Egg();
+			}
+		}
+		return eggBoxStack;
 	}
+	
+	public Egg[][] deliverEgg(int quanOfEgg, int exception) {
+		exception=this.exception;
+		
+	}
+	
+	
 }
